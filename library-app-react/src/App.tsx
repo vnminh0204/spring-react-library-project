4de59a10c3459da6ef494ec52c7a3d1cfbd7a6df
Minh Vo <108131130+vnminh0204@users.noreply.github.com>
2024-05-09 23:29:16 +0700

    clean up

--- library-app-react/src/App.tsx
+++ library-app-react/src/App.tsx
@@ -3,10 +3,8 @@ import './App.css';
 import {HomePage} from "./pages/HomePage/HomePage";
 import {Navbar} from "./components/NavbarAndFooter/Navbar";
 import {Footer} from "./components/NavbarAndFooter/Footer";
-import {BASE_URL} from "./configs/env";
 
 function App() {
-    console.log(BASE_URL);
     return (
         <div>
             <Navbar/>
