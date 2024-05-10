edfcffa2a814900ca90346ac452c55312611e748
Minh Vo <nhat-minh.vo@compax.at>
2024-05-10 17:09:42 +0700

    feat: add search book page + components

--- library-app-react/src/components/NavbarAndFooter/Navbar.tsx
+++ library-app-react/src/components/NavbarAndFooter/Navbar.tsx
@@ -4,7 +4,7 @@ export const Navbar = () => {
   return (
       <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
         <div className='container-fluid'>
-          <span className='navbar-brand'>Luv 2 Read</span>
+          <span className='navbar-brand'>Our Bookstore</span>
           <button className='navbar-toggler' type='button'
                   data-bs-toggle='collapse' data-bs-target='#navbarNavDropdown'
                   aria-controls='navbarNavDropdown' aria-expanded='false'
@@ -31,65 +31,3 @@ export const Navbar = () => {
       </nav>
   )
 }
-// import { Link, NavLink } from "react-router-dom";
-// import { useOktaAuth } from '@okta/okta-react';
-// import { SpinnerLoading } from "../Utils/SpinnerLoading";
-//
-// export const Navbar = () => {
-//
-//   const { oktaAuth, authState } = useOktaAuth();
-//
-//   if (!authState) {
-//     return <SpinnerLoading />
-//   }
-//
-//   const handleLogout = async () => oktaAuth.signOut();
-//
-//   console.log(authState);
-//
-//   return (
-//     <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
-//       <div className='container-fluid'>
-//         <span className='navbar-brand'>Luv 2 Read</span>
-//         <button className='navbar-toggler' type='button'
-//           data-bs-toggle='collapse' data-bs-target='#navbarNavDropdown'
-//           aria-controls='navbarNavDropdown' aria-expanded='false'
-//           aria-label='Toggle Navigation'
-//         >
-//           <span className='navbar-toggler-icon'></span>
-//         </button>
-//         <div className='collapse navbar-collapse' id='navbarNavDropdown'>
-//           <ul className='navbar-nav'>
-//             <li className='nav-item'>
-//               <NavLink className='nav-link' to='/home'>Home</NavLink>
-//             </li>
-//             <li className='nav-item'>
-//               <NavLink className='nav-link' to='/search'>Search Books</NavLink>
-//             </li>
-//             {authState.isAuthenticated &&
-//               <li className='nav-item'>
-//                 <NavLink className='nav-link' to='/shelf'>Shelf</NavLink>
-//               </li>
-//             }
-//             {authState.isAuthenticated && authState.accessToken?.claims?.userType === 'admin' &&
-//               <li className='nav-item'>
-//                 <NavLink className='nav-link' to='/admin'>Admin</NavLink>
-//               </li>
-//             }
-//           </ul>
-//           <ul className='navbar-nav ms-auto'>
-//             {!authState.isAuthenticated ?
-//               <li className='nav-item m-1'>
-//                 <Link type='button' className='btn btn-outline-light' to='/login'>Sign in</Link>
-//               </li>
-//               :
-//               <li>
-//                 <button className='btn btn-outline-light' onClick={handleLogout}>Logout</button>
-//               </li>
-//             }
-//           </ul>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// }
