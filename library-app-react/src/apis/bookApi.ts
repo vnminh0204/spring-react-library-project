adc5975936c2c8e1f4fbbc09c2102d0707eaad08
Minh Vo <nhat-minh.vo@compax.at>
2024-05-13 15:57:17 +0700

    feat: clean up

--- library-app-react/src/apis/bookApi.ts
+++ library-app-react/src/apis/bookApi.ts
@@ -10,14 +10,6 @@ export const BookApi = {
         return response.json();
     },
 
-    async getBookContainingSearch(search: string, offset = 0, limit = 9) {
-        const response = await fetch(`${BASE_URL}/books/search/findByTitleContaining?title=${search}&offset=${offset}&limit=${limit}`);
-        if (!response.ok) {
-            throw new Error("Failed to fetch the books");
-        }
-        return response.json();
-    },
-
     async getBookById(bookID: number) {
         const response = await fetch(`${BASE_URL}/books/${bookID}`);
         return response.json();
