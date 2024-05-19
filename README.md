dc4944a4edbbe90479aced2b259a40c10a40ecd6
Minh Vo <108131130+vnminh0204@users.noreply.github.com>
2024-05-19 20:47:08 +0700

    feat: https set up

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # library-project
 
 Generate ssl certificate for https connection
+Front End React
 https://github.com/darbyluv2code/fullstack-react-and-springboot/blob/main/bonus-content/openssl-setup.md
+Back End Spring Boot
+https://github.com/darbyluv2code/fullstack-react-and-springboot/blob/main/bonus-content/keytool-steps.md
--- library-app-react/src/configs/oktaConfig.ts
+++ library-app-react/src/configs/oktaConfig.ts
@@ -3,7 +3,7 @@ import {OKTA_CLIENT_ID, OKTA_ISSUER} from "./env";
 export const oktaConfig = {
     clientId: `${OKTA_CLIENT_ID}`,
     issuer: `https://${OKTA_ISSUER}/oauth2/default`,
-    redirectUri: 'http://localhost:3000/login/callback',
+    redirectUri: 'https://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true,
